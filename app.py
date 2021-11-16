d1b676de9bc968d1aa95e43b1e21c561697995e5
d99mli <d99mli@gmail.com>
2021-11-16 17:46:39 +0000

    add routing to articles with bug

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
 from flask import Flask, render_template
+from data import Articles
 
 app = Flask(__name__)
+
+Articles = Articles()
 
 
 @app.route('/')
 def index():
     return render_template('home.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/articles')
+def articles():
+    return render_template('articles.html', articles=Articles)
+
+
+@app.route('/article/<page_id>')
+def article(page_id):
+    return render_template('article.html', id=page_id)
 
 
 if __name__ == '__main__':
