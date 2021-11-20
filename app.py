5204e71b0aa3e0f8852b2a802cb69b9bcf4e7c20
d99mli <d99mli@gmail.com>
2021-11-20 14:04:15 +0000

    MongoDB connection added

--- app.py
+++ app.py
@@ -2,17 +2,23 @@ import os
 from flask import (
     Flask, render_template, flash, redirect,
     url_for, session, request, logging)
-# from flask_pymongo import PyMongo
-from data import Articles
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from passlib.hash import sha256_crypt
 from wtforms import (
     Form, StringField, TextAreaField, PasswordField, validators)
-from passlib.hash import sha256_crypt
+from data import Articles
 if os.path.exists("env.py"):
     import env
 
 app = Flask(__name__)
 
 # MongoDB Connection Config
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
 
 Articles = Articles()
 
@@ -37,6 +43,12 @@ def article(page_id):
     return render_template('article.html', id=page_id)
 
 
+@app.route("/get_users")
+def get_users():
+    users = mongo.db.users.find()
+    return render_template('users.html', users=users)
+
+
 class RegisterForm(Form):
     name = StringField('Name', [validators.Length(min=1, max=50)])
     username = StringField('Username', [validators.Length(min=4, max=25)])
@@ -52,7 +64,8 @@ class RegisterForm(Form):
 def register():
     form = RegisterForm(request.form)
     if request.method == 'POST' and form.validate():
-
+        # name = form.name.mongo
+        # email =
         return render_template('register.html')
     return render_template('register.html', form=form)
 
