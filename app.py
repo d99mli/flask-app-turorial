6f5f0803a1643d9549debbf0b7333115c27c2b57
d99mli <d99mli@gmail.com>
2021-12-08 22:16:00 +0000

    add delete func and account page

--- app.py
+++ app.py
@@ -114,7 +114,7 @@ def login():
                 flash("Welcome, {}".format(
                     request.form.get("username")), 'success')
 # return redirect(url_for('profile', username=session["user"]))
-                return redirect(url_for('dashboard', session=["user"]))
+                return redirect(url_for('account'))
             else:
                 # invalid password match
                 flash("Incorrect Username and/or Password")
@@ -158,20 +158,51 @@ def dashboard():
     return render_template('dashboard.html')
 
 
-# Profile page (same as dashboard)
-@app.route("/profile/<username>", methods=["GET", "POST"])
-def profile(username):
-    # grab the session user's username from db
-    username = mongo.db.users.find_one(
-        {"username": session['user']})["username"]
-    return render_template("profile.html", username=username)
+# User Account form class
+class UpdateAccountForm(Form):
+    name = StringField('Name', [validators.Length(min=1, max=50)])
+    username = StringField('Username', [validators.Length(
+        min=4, max=25, message="Username must be between 4 and 25 characters.")
+    ])
+    email = StringField('Email', [
+        validators.Length(min=6, max=35),
+        validators.Regexp(
+            '^[a-zA-Z0-9.!#$%&*+/=?_~-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$',
+            message="Must be a valid e-mail")
+    ])
+
+
+# Profile Account page (for logged in users)
+# @app.route("/account", methods=["GET", "POST"])
+# def account():
+#     # grab the session user's username from db
+#     user = mongo.db.users.find_one(
+#         {"username": session['user']})
+#     user_image = url_for('static', filename='pics/userimage.jpg')
+#     form = UpdateAccountForm(request.form)
+#     if request.method == 'POST' and form.validate():
+#         update_account = {
+#                 "name": request.form.get("name").lower(),
+#                 "email": request.form.get("email").lower(),
+#                 "username": request.form.get("username").lower()
+#             }
+#         # insert_one requires an dictionary to store info in mongoDB
+#         mongo.db.users.update(update_account)
+#         return redirect(url_for('account'))
+#     return render_template(
+#         "account.html", user=user, user_image=user_image, form=form)
 
 
 # Article form class
 class ArticleForm(Form):
     title = StringField('Title', [validators.Length(min=1, max=100)])
-    body = TextAreaField('Body',
-        [validators.Length(min=10)], render_kw={'rows': 20})
+    body = TextAreaField('Body', [
+        validators.Length(min=10)], render_kw={'rows': 20})
+
+
+# Category form class
+class CategoryForm(Form):
+    category = StringField('Category', [validators.Length(min=3, max=25)])
 
 
 # Add article (to db)
@@ -230,6 +261,59 @@ def delete_article(article_id):
 def get_categories():
     categories = list(mongo.db.categories.find().sort("category_name", 1))
     return render_template("categories.html", categories=categories)
+
+
+@app.route('/add_category', methods=["GET", "POST"])
+@is_logged_in
+def add_category():
+    form = CategoryForm(request.form)
+    if request.method == 'POST' and form.validate():
+
+        one_category = {
+            "category_name": request.form.get('category').lower(),
+            "create_date": datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        }
+
+        mongo.db.categories.insert_one(one_category)
+        flash("Category Successfully Created!", 'success')
+        return redirect(url_for('dashboard'))
+
+    return render_template("add_category.html", form=form)
+
+
+# Edit Category
+@app.route("/edit_category/<category_id>", methods=["GET", "POST"])     #  route decorator which is passed with variable of category_id
+@is_logged_in
+def edit_category(category_id):
+    category = mongo.db.categories.find_one({"_id": ObjectId(category_id)})
+    form = CategoryForm(request.form, category)
+
+    # populate category form fields
+    form.category.data = category['category_name']
+
+    if request.method == 'POST' and form.validate():
+
+        one_category = {
+            "category_name": request.form.get('category').lower(),
+            "create_date": datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        }
+
+        mongo.db.categories.update(
+            {"_id": ObjectId(category_id)}, one_category)
+        flash("Category Successfully Updated", 'success')
+        return redirect(url_for('get_categories'))
+
+    return render_template(
+        "edit_category.html", form=form, category=category)
+
+
+# Delete Category
+@app.route("/delete_category/<category_id>")
+def delete_category(category_id):
+    mongo.db.categories.remove({"_id": ObjectId(category_id)})
+    flash("Category Successfully removed!")
+    return redirect(url_for('get_categories'))
+
 
 
 if __name__ == '__main__':
